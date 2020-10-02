aad8dfd69f521166ff8b9f1ec7ab55ec4f783f9c
Pankit Kapadia <pankit_1990@yahoo.com>
2020-10-02 12:52:09 +0530

    google analytics script update

--- index.php
+++ index.php
@@ -19,14 +19,15 @@ include "functions.php";
 	<!-- https://coolors.co/2d3142-bfc0c0-ffffff-ef8354-4f5d75 -->
 	<!-- new suggestions -->
 	<!-- https://coolors.co/f79f79-adc698-f7d08a-fffcf7-e3f09b -->
-	<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 	
-	ga('create', 'UA-106385923-1', 'auto');
-	ga('send', 'pageview');
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106385923-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-106385923-1');
 	</script>
 </head>
 <body>
